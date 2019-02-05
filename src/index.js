6edebe365c2c5666ce477c0875893af01a77a777
Ruslan Komandyshko <eshk3n@yandex.ru>
2019-02-05 12:09:29 +0000

    remv return

--- src/index.js
+++ src/index.js
@@ -4,4 +4,3 @@ const welcomeMsg = console.log('Welcome to the Brain Games!\n');
 const name = readlineSync.question('May I have your name?\n');
 const hello = console.log('Hi, ${name}!\n');
 
-return;
