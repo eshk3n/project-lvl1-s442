b3d8a607865e8bdd07046ddd31d346abb13531eb
Ruslan Komandyshko <eshk3n@yandex.ru>
2019-02-05 12:07:52 +0000

    add return

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import readlineSync from 'readline-sync';
 const welcomeMsg = console.log('Welcome to the Brain Games!\n');
 const name = readlineSync.question('May I have your name?\n');
 const hello = console.log('Hi, ${name}!\n');
+
+return;
