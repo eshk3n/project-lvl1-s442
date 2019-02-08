238dd0b9f456294aebfe954f77f502b79fe77036
Ruslan Komandyshko <eshk3n@yandex.ru>
2019-02-08 12:22:16 +0000

    e

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
 
-import {game} from '..';
+import { game } from '../';
 game();
-
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import readlineSync from 'readline-sync';
 
 export default game = () => {
-  const welcomeMsg = console.log('Welcome to the Brain Games!\n');
+  console.log('Welcome to the Brain Games!\n');
   const name = readlineSync.question('May I have your name?\n');
   console.log('Hi, ${name}!\n');
-}
-
+};
