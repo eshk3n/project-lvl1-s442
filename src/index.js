be7392a66e3a0ff8e934d05c4d1d6dee9f8b3873
Ruslan Komandyshko <eshk3n@yandex.ru>
2019-02-08 12:29:13 +0000

    f

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export default game = () => {
+export const game = () => {
   console.log('Welcome to the Brain Games!\n');
   const name = readlineSync.question('May I have your name?\n');
   console.log('Hi, ${name}!\n');
