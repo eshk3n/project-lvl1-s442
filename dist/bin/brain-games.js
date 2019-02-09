5290a101bd4a69c6b908d684f28838b90c1b4b35
Ruslan Komandyshko <eshk3n@yandex.ru>
2019-02-10 01:57:00 +0300

    d

--- dist/bin/brain-games.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-
-var _ = _interopRequireDefault(require(".."));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-(0, _.default)();
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9iaW4vYnJhaW4tZ2FtZXMuanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7OztBQUVBOzs7O0FBRUEiLCJzb3VyY2VzQ29udGVudCI6WyIjIS91c3IvYmluL2VudiBub2RlXG5cbmltcG9ydCBnYW1lIGZyb20gJy4uJztcblxuZ2FtZSgpO1xuIl19
