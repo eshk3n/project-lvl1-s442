30f840cc529f18777224221d337b2cd7353cb949
Ruslan Komandyshko <eshk3n@yandex.ru>
2019-02-10 15:39:39 +0300

    f

--- src/index.js
+++ src/index.js
@@ -23,9 +23,8 @@ export const even = () => {
   }
 };
 
-const game = () => {
+export const game = () => {
   console.log('Welcome to the Brain Games!\n');
   //  const name = readlineSync.question('May I have your name?\n');
   console.log(`Hello, ${name}!`);
 };
-export default game;
